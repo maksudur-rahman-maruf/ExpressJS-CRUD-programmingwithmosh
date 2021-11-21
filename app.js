42efb99bc7ae1d297a24cd428c2cd712bc0aefd5
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2021-11-21 22:17:12 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 const express = require('express');
+const employees = require('./employee');
 
 const app = express();
+app.use(express.json());
+
 
 app.get('/', function(req, res) {
     res.send(
-        {
-            id: 505,
-            name: 'Maksudur Rahman Maruf',
-            Company: 'Dynamic Solution Innovators',
-            Team: ['Banbeis', 'InfoImage'],
-            Designation: 'Junior Software Engineer'
-        }
+        employees[0]
     );
 });
 
-app.listen(3000);
+app.get('/api/posts/:year/:month', function(req, res) {
+    res.send(
+       [req.params, req.query]
+    );
+});
+
+app.get('/api/employees', function(req, res) {
+    res.send(
+       employees
+    );
+});
+
+app.get('/api/employee/:id', function(req, res) {
+    const employee = employees.find(emp => emp.id === parseInt(req.params.id));
+    if(!employee) res.status(404).send('The Employee with the given ID is not found!');
+    return res.send(employee);
+});
+
+app.post('/api/employees', (req, res) => {
+    const employee = {
+        id: employees.length + 1,
+        name: req.body.name,
+        company: req.body.company,
+        team: req.body.team,
+        designation: req.body.designation
+    }
+    employees.push(employee);
+
+    res.send(employee);
+
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ employee.js
@@ -0,0 +1,33 @@
+
+const employees = [
+    {
+        id: 1,
+        name: 'Maksudur Rahman Maruf',
+        company: 'Dynamic Solution Innovators',
+        team: ['Banbeis', 'InfoImage'],
+        designation: 'Junior Software Engineer'
+    },
+    {
+        id: 2,
+        name: 'Rashid Abrar Riyad',
+        company: 'Dynamic Solution Innovators',
+        team: ['InfoImage'],
+        designation: 'Junior Software Engineer'
+    },
+    {
+        id: 3,
+        name: 'Nishat Afroz',
+        company: 'Dynamic Solution Innovators',
+        team: ['DRIMS'],
+        designation: 'Junior Software Engineer'
+    },
+    {
+        id: 4,
+        name: 'Ashef Shahrior',
+        company: 'Dynamic Solution Innovators',
+        team: ['DPE'],
+        designation: 'Junior Software Engineer'
+    }
+];
+
+module.exports = employees;
